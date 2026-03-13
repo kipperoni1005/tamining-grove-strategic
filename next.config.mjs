cf51b2869692f9be53c80c25e18691327710bd1b
kiprono1005 <kchemweno@vt.edu>
2026-03-13 00:21:37 -0400

    Exclude cache from output

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = { 
+  reactStrictMode: true,
+  outputFileTracingExcludes: {
+    '*': ['**/*.map', '**/.cache/**/*']
+  }
+};
 export default nextConfig;
